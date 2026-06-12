44c47e6791110ad487bac4c0369ef513d8a67d30
visidhantworks <158760995+visidhantworks@users.noreply.github.com>
2026-06-12 22:05:21 +0530

    Add database connection function
    Added a function to establish a database connection.

--- configure.php
+++ configure.php
@@ -4,3 +4,12 @@ define('DB_HOST', 'sql302.infinityfree.com');
 define('DB_USER', 'if0_42099223');
 define('DB_PASS', 'your_password');
 define('DB_NAME', 'if0_42099223_myos');
+function getDbConnection() {
+    return new mysqli(
+        DB_HOST,
+        DB_USER,
+        DB_PASS,
+        DB_NAME
+    );
+}
+?>
