63b0471ac0ad42276fff07524afb5bd5b45df078
visidhantworks <sidhant132600@gmail.com>
2026-06-12 22:14:30 +0530

    Update configure

--- configure.php
+++ configure.php
@@ -1,9 +1,11 @@
+// configure.php
 <?php
 
 define('DB_HOST', 'sql302.infinityfree.com');
 define('DB_USER', 'if0_42099223');
-define('DB_PASS', 'your_password');
+define('DB_PASS', '...');
 define('DB_NAME', 'if0_42099223_myos');
+
 function getDbConnection() {
     return new mysqli(
         DB_HOST,
